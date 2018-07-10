460f8b28012f981c9adb8334b027f05932ba4fef
Jason Schultz <jetson23@yahoo.com>
2018-07-09 20:04:13 -0500

    Updates to client and server, 2 commands working: ethtool, ifconfig

--- client.py
+++ client.py
@@ -1,9 +1,23 @@
 #!/bin/python3
 
 import socket
+import struct
+
+def send_data(sock,data): #Implement a networking protocol
+    data_len = len(data)
+    sock.send(struct.pack("!I",data_len))
+    sock.send(data)
+    return
 
 if __name__ == '__main__':
     # Client program to connect to server and do commands
+    
+    # List of commands to perform on server
+    command_list = ["ethtool",
+                    "ifconfig",
+                    "wrfile",
+                    "rdfile",
+                    "close"]
     
     # Create a socket object
     s = socket.socket()         
@@ -12,9 +26,42 @@ if __name__ == '__main__':
     port = 12345               
  
     # connect to the server on local computer
-    s.connect(('127.0.0.1' port))
+    s.connect(('127.0.0.1', port))
  
-    # receive data from the server
-    print s.recv(1024)
+    cont = True
+    
+    while (cont == True):
+        # ask for a command
+        command = input("Enter a command: ")
+        print (command)
+
+        if command == "close":
+            s.shutdown(1)
+            cont = False
+        elif command == "ifconfig":
+            # receive data from the server
+            #data_len = len(command)
+            #sock.send(struct.pack("!I",data_len))
+            s.send(command.encode())
+            #send_data(s, command.encode())
+            print ("recv result") 
+            result = b''
+            while result == b'':
+                result = s.recv(2048)
+                print (result)
+                
+            print (resuolt.decode())
+        elif "ethtool" in command:
+            device = input("Device: ")
+            cmd = "ethtool " + device
+            print (cmd)
+            s.send(cmd.encode())
+            print ("recv ethtool")
+            
+            
+        else:
+            print("invalid command")
+    
     # close the connection
     s.close()       
+    print("closed; end")
--- server.py
+++ server.py
@@ -1,42 +1,93 @@
 #!/bin/python3
 
+import os
 import socket
+import struct
+import subprocess
 
 if __name__ == '__main__':
     # Server to listen for connections and process commands
-              
+     
+    # List of commands to perform on server
+    command_list = ["ethtool",
+                    "ifconfig",
+                    "wrfile",
+                    "rdfile",
+                    "close"]
+
     # next create a socket object
-    s = socket.socket()         
-    print "Socket successfully created"
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print ("Socket successfully created")
  
     # reserve a port on your computer in our
     # case it is 12345 but it can be anything
     port = 12345               
  
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # Next bind to the port
     # we have not typed any ip in the ip field
     # instead we have inputted an empty string
-    # this makes the server listen to requests 
+    # this makes the server listen to requests man 
     # coming from other computers on the network
     
     # Add IP address
     s.bind(('127.0.0.1', port))        
-    print "socket binded to %s" %(port)
+    print ("socket binded to", port)
  
     # put the socket into listening mode
     s.listen(5)     
-    print "socket is listening"           
+    print ("socket is listening")          
  
-    # a forever loop until we interrupt it or 
-    # an error occurs
-    while True:
- 
-        # Establish connection with client.
-        conn, addr = s.accept()     
-        print 'Got connection from', addr
- 
-        # send a thank you message to the client. 
-        conn.send('Thank you for connecting')
- 
-        # Close the connection with the client
-        conn.close()
+    # Establish connection with client.
+    conn, addr = s.accept()     
+    print ('Got connection from', addr)
+
+    # a forever loop until we stop or an error occurs
+    cont = True
+    while (cont == True):
+        # get command from the client. 
+        print ("b4 recv")
+        cmd = conn.recv(1024)
+        #data_len, = struct.unpack("!I",s.recv(4))
+        #cmd = s.recv(data_len)
+        
+        print (cmd)
+        if cmd == b'':
+            print ("shutdown")
+            s.shutdown(1)
+            s.close()
+            break
+        
+        print ("deccode")
+        cmds = cmd.decode().strip()
+        #cmds = str(cmdd)
+        print (cmds)
+        
+
+        #if cmd.decode("utf-8").strip() == "IFCONFIG":
+        if cmds == 'ifconfig':
+            print ("ifconfig")
+            p = subprocess.Popen("ifconfig", stdout=subprocess.PIPE, shell=True)
+            (output, err) = p.communicate()
+            print (output.decode())
+            conn.send(output)  # already encoded here
+            
+        elif 'ethtool' in cmds:
+            print ("ethtool")
+            print (cmds)
+            
+            p = subprocess.Popen(cmds, stdout=subprocess.PIPE, shell=True)
+            (output, err) = p.communicate()
+            print (output.decode())
+            conn.send(output)  # already encoded here
+            
+        elif cmds == 'close':
+            cont = False
+        else:
+            print ("command not recognized")
+            cont = False
+           
+		
+    # Close the connection with the client
+    conn.close()
+    print ("Conn closed")
